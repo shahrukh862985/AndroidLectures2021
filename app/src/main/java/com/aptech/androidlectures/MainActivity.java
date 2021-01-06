a0f83db19e32fafe32ed5123cf113a869c2560b9
shahrukh862985 <shah353250@gmail.com>
2021-01-06 20:46:13 +0500

    ListView with ArrayAdapter Lecture

--- app/src/main/java/com/aptech/androidlectures/MainActivity.java
+++ app/src/main/java/com/aptech/androidlectures/MainActivity.java
@@ -2,13 +2,53 @@ package com.aptech.androidlectures;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.os.Bundle;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView listView;
+    ArrayList<String> employees;
+    ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.listview);
+        employees = new ArrayList<>();
+        employees.add("Asad");
+        employees.add("Asim");
+        employees.add("Ali");
+        employees.add("Wajahat");
+        employees.add("asif");
+        employees.add("Amir");
+        employees.add("Adil");
+        employees.add("Asad");
+        employees.add("Asim");
+        employees.add("Ali");
+        employees.add("Wajahat");
+        employees.add("asif");
+        employees.add("Amir");
+        employees.add("Adil");
+        employees.add("Asad");
+        employees.add("Asim");
+        employees.add("Ali");
+        employees.add("Wajahat");
+        employees.add("asif");
+        employees.add("Amir");
+        employees.add("Adil");
+        employees.add("Asad");
+        employees.add("Asim");
+        employees.add("Ali");
+        employees.add("Wajahat");
+        employees.add("asif");
+        employees.add("Amir");
+        employees.add("Adil");
+
+        adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,employees);
+        listView.setAdapter(adapter);
     }
 }
